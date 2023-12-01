5c5e0b579b494e1ae1f9663a449081e662968196
Elineum <you@example.com>
2023-12-01 19:01:07 +0200

    Happy birthday!

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const swiperImg = new Swiper(".swiper-img", {
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperVid = new Swiper(".swiper-video", {
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+const swiperLast = new Swiper(".swiper-video-last", {
+  direction: "horizontal",
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
